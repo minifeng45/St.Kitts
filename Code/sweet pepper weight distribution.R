815e0a418e8ab9876ac1ba063952e5f5cb1b49b9
何宇峰 <supermonk00@heyufengs-MacBook-Pro.local>
2020-03-26 16:18:13 -0400

    Coronavirus in St. Kiits no

--- Code/sweet pepper weight distribution.R
+++ Code/sweet pepper weight distribution.R
@@ -4,21 +4,52 @@ require(ggthemes)
 
 # move climate data file to desktop
 
-setwd("/Users/supermonk00/desktop")
+setwd("/Users/supermonk00/Desktop/academy/programing/R/St.Kitts/Code")
+source("Sweetpepper_data_insert.R")
+
+# total harvest data
+
+purple_total = rbind(purple_before_trim,purple_notrim,purple_trim)[,-1]
+red_total = rbind(red_before_trim,red_notrim,red_trim)[,-1]
 
 
 ### weight distribution (red vs. purple)
+# weekly weight dist.
+weekly_totalweight = function(date){
+  
+  weekly_P       =purple_total[which(purple_total$Date == date),]
+  weekly_R         =red_total[which(red_total$Date == date),]
+ 
+  totalweight = c(sum(weekly_P$`Weight(g)`),sum(weekly_R$`Weight(g)`))
+  return(totalweight)
+}
 
-weightdat = read.csv("Sweetpepperwieght.csv",header = T)
-head(weightdat) #check data
-colnames(weightdat) = c("Red","Purple")
+daylayout = matrix(harvest_day, nrow =1)
+weekly_harvest = apply(daylayout, 2, weekly_totalweight)
 
-#data transfer 
+Harvest = as.data.frame(cbind(harvest_day,t(weekly_harvest)))
+colnames(Harvest) = c("Date","Purple","Red")
 
-dat = data.frame(
-  Variety = factor(c(rep("Red",length(na.omit(weightdat$Red))),
-                   rep("Purple",length(weightdat$Purple)))),
-  Weight = c(na.omit(weightdat$Red), weightdat$Purple))
+
+
+
+
+
+
+
+pest_on_fruit = purple_before_trim[which(purple_before_trim$Pest==1),]
+mean(pest_on_fruit$`Weight(g)`)
+mean(purple_before_trim$`Weight(g)`)
+
+pest_on_fruit = purple_before_trim[which(purple_before_trim$Rotten==1),]
+mean(pest_on_fruit$`Weight(g)`)
+mean(purple_before_trim$`Weight(g)`)
+
+Rotten_on_fruit = red_before_trim[which(red_before_trim$Rotten==1),]
+mean(Rotten_on_fruit$`Weight(g)`)
+mean(red_before_trim$`Weight(g)`)
+
+
 
 ###histogram 
 library(ggplot2)
@@ -28,13 +59,15 @@ library(plyr)
 mu <- ddply(dat, "Variety", summarise, grp.mean=mean(Weight))
 
 
+
 ## two variety compare
 #split by color
 #position : identity
 #break = 3g
 # dashed line :  mu value
-p<-ggplot(dat, aes(x=Weight, fill = Variety, color=Variety)) +     # fill color follow category"Variety"
-  geom_histogram(position="identity",binwidth = 3, alpha = 0.5)+   # separate data, bar width, transparency
+p<-ggplot(Harvest, aes(x=Date, fill = Variety, color=Variety)) +     # fill color follow category"Variety"
+  geom_histogram(position="identity",binwidth = 3, alpha = 0.5)     # separate data, bar width, transparency
+
   geom_vline(data=mu, aes(xintercept=grp.mean, color=Variety),     # the mean line
              linetype="dashed")+
   theme(legend.position="top")+                                    # the label's position
